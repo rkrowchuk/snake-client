35a73bbfeaee2127160f4ef0bbfa914ed2674d21
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-16 14:05:18 -0800

    module.exports

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  conn.on("connect", () => {
+    console.log("connected");// code that does something when the connection is first established
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);// code that does something when data is sent
+  });
+
+  return conn;
+};
+
+module.exports = { net, connect };
