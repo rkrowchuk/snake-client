b06ca93304a5725799868d7882afebc60b1e66c8
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-16 14:54:00 -0800

    create handleUserInput

--- play.js
+++ play.js
@@ -3,3 +3,17 @@ const { net, connect } = require("./client");
 console.log("Connecting ...");
 connect();
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+    if (key === '\u0003') {
+      process.exit();
+    }
+};
