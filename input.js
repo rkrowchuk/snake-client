cae341f2aa116c139f1fef90b023969cb00ebead
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-16 16:39:10 -0800

    add key functions for movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,9 +10,21 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (key) {  
-    if (key === '\u0003') {
-      process.exit();
+const handleUserInput = function (key) { 
+  if (key === '\u0077') {
+    connection.write("Move: up");
+  }  
+  if (key === '\u0061') {
+    connection.write("Move: left");
+  }  
+  if (key === '\u0073') {
+    connection.write("Move: down");
+  }  
+  if (key === '\u0064') {
+    connection.write("Move: right");
+  }  
+  if (key === '\u0003') {
+    process.exit();
     }
 };
 
